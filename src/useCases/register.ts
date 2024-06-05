151e9c6d00a6b121ca9a9b57cba35895df455767
frmatias <vandembergneto@gmail.com>
2024-06-04 22:53:48 -0300

    abstract repository

--- src/useCases/register.ts
+++ src/useCases/register.ts
@@ -9,22 +9,19 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: { email },
-  })
-  if (userWithSameEmail) {
-    throw new Error(`User ${userWithSameEmail.name} already exists`)
+class RegisterUseCase {
+  constructor(private usersRepository: UsersPrismaRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+    if (userWithSameEmail) {
+      throw new Error(`User ${userWithSameEmail.name} already exists`)
+    }
+    const password_hash = await hash(password, 5)
+    await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
   }
-  const password_hash = await hash(password, 5)
-  const usersPrismaRepository = new UsersPrismaRepository()
-  await usersPrismaRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
